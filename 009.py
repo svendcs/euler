dce74ca048c84c7f075c560d0037047184e320e0
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2014-06-30 11:48:24 +0200

    Problem #9 fix.

--- 009.py
+++ 009.py
@@ -11,10 +11,12 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
-n = 1000
-for a in range(1, n):
-    for b in range(1, n-a):
-        c = 1000 - a - b
-        if a*a + b*b == c*c :
-            print(a*b*c)
-            break
+def triplet(n):
+    for a in range(1, n):
+        for b in range(1, n-a):
+            c = 1000 - a - b
+            if a*a + b*b == c*c :
+                return a*b*c
+    return 0
+
+print(triplet(1000))
